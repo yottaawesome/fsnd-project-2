4d624f09c49cfbc555473620c2bf260f04fee1be
Vasilios Magriplis <etameson@gmail.com>
2019-04-11 00:46:39 +0930

    * Minor changes

--- src/main.py
+++ src/main.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def restaurants():
-    return render_template("index.html", restaurants=restaurants)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = "127.0.0.1", port = 5000)
