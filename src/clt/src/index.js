cecef8ccaa5c1c846830692b2cae9f07814a2741
Vasilios Magriplis <etameson@gmail.com>
2019-04-25 17:52:49 +0930

    Do client work: * Swap to HashRouter to fix 404 issues. * Minor style changes. * Remove title from index.html.

--- src/clt/src/index.js
+++ src/clt/src/index.js
@@ -1,26 +1,29 @@
 import { render, Component } from 'inferno';
-import { BrowserRouter, Route, Switch, Link } from 'inferno-router';
+import { HashRouter, Route, Switch, Link } from 'inferno-router';
 import Home from './home';
 import Login from './login';
 import './index.scss';
 
-const MyWebsite = () => (
-  <BrowserRouter>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+const MainClient = () => (
+  <div>
+    <h1>Welcome to your Bookshelf!</h1>
+    <HashRouter>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </HashRouter>
+  </div>
 );
 
 render(
-  <MyWebsite />,
+  <MainClient />,
   document.getElementById("app")
 );
