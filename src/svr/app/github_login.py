887ca617aa7b1294f8c8bbb4123722df636de7c7
Vasilios Magriplis <etameson@gmail.com>
2019-04-29 01:52:27 +0930

    Minor change

--- src/svr/app/github_login.py
+++ src/svr/app/github_login.py
@@ -4,10 +4,32 @@ from flask import (Flask, render_template, url_for,
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import random, string, httplib2, json, requests
+import base64
 
 from .flask_app import main_app, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GOOGLE_CLIENT_ID
 from db import Dal, dal_factory, User
 dal_fct = dal_factory()
+
+def revoke_token():
+    # returns 404
+    #url = 'https://api.github.com/applications/{}/tokens/{}'.format(GITHUB_CLIENT_ID, access_token)
+    #secret = base64.urlsafe_b64encode('{}:{}'.format(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET).encode())
+    #headers = { 'Authorization': 'Basic {}'.format(secret)}
+    #revoke_response = requests.delete(url, headers=headers)
+    
+    # returns 401
+    # apparently, requires the user to enter their password, which is silly
+    # https://stackoverflow.com/questions/17217750/revoking-oauth-access-token-results-in-404-not-found
+    #url = 'https://api.github.com/authorizations/{}'.format(access_token)
+    #secret = base64.urlsafe_b64encode('{}:{}'.format(GITHUB_CLIENT_SECRET).encode())
+    #headers = { 'Authorization': 'Basic {}'.format(secret)}
+    #revoke_response = requests.delete(url, headers=headers)
+    #if revoke_response.status_code == 204:
+    #    print("Token revoked")
+    #else:
+    #    print(revoke_response.status_code)
+    #    print(revoke_response.text)
+    pass
 
 # Code adapted from https://developer.github.com/v3/guides/basics-of-authentication/
 # GitHub endpoints: https://developer.github.com/apps/building-github-apps/identifying-and-authorizing-users-for-github-apps/
@@ -16,7 +38,7 @@ def github_callback():
     # this is the temporary GitHub code passed in via the querystring
     session_code = request.args.get('code')
     
-    # swap the the temportary code for an access token
+    # swap the the temporary code for an access token
     url = 'https://github.com/login/oauth/access_token'
     data = {
         'client_id': GITHUB_CLIENT_ID,
@@ -45,5 +67,5 @@ def github_callback():
                                     user_details_data['avatar_url'])
             dal.flush()
         login_session['user'] = user_record.serialize
-    
+
     return redirect(url_for("home"), code=303)
